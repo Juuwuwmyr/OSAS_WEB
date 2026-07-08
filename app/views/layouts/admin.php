1c74384ca7dc0c5592826630bc9f6ae92c1a9d60
Jumyr <morenojumyr0@gmail.com>
2026-07-08 11:21:12 -0700

    fix mini design

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -40,7 +40,6 @@ $notificationCount = 1; // Can be dynamic later
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes, viewport-fit=cover">
   <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="default">
   <meta name="mobile-web-app-capable" content="yes">
   <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
   <title>E-OSAS SYSTEM</title>
